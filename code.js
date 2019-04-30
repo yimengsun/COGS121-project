d22314b1648647868772cc0994c5c5ae2242fe28
Ignacio Becerra <i1becerr@ucsd.edu>
2019-04-29 18:33:57 -0700

    Working on results page

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
-function searchZip() {
-
-	console.log(document.getElementById('zip-code').value);
+function openTab(evt, TabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(TabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
